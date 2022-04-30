34325872ddce8f16cc29c95dcf06d72b48e92995
Бернова Анна <bast33@yandex.ru>
2022-04-30 13:44:00 +0300

    сократили вьюху редактирования поста

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -72,16 +72,13 @@ def post_create(request):
 
 
 def post_edit(request, post_id):
-    post = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, id=post_id)
     if request.user != post.author:
         return redirect('posts:post_detail', post_id)
     else:
-        if request.method != 'POST':
-            form = PostForm(instance=post)
-        else:
-            form = PostForm(instance=post, data=request.POST)
-            if form.is_valid():
-                form.save()
-                return redirect('posts:post_detail', post_id)
+        form = PostForm(request.POST or None, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:post_detail', post_id)
         context = {'form': form, 'is_edit': True}
         return render(request, 'posts/create_post.html', context)
