11d9571ebc22863238fbda26b1124677f81c3b4a
Бернова Анна <bast33@yandex.ru>
2022-05-06 14:00:04 +0300

    тесты для verbose_name и help_text

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -14,11 +14,18 @@ class Group(models.Model):
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(
+        'Текст поста',
+        help_text='Введите текст поста'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
+        verbose_name='Автор',
         related_name='posts'
     )
     group = models.ForeignKey(
@@ -26,7 +33,9 @@ class Post(models.Model):
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name='posts_group'
+        related_name='posts_group',
+        verbose_name='Группа',
+        help_text='Группа, к которой будет относиться пост'
     )
 
     def __str__(self):
--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -32,3 +32,29 @@ class PostModelTest(TestCase):
         post = PostModelTest.post
         expected_object_name = 'Тестовый пост б'
         self.assertEqual(expected_object_name, str(post))
+
+    def test_post_verbose_name(self):
+        """verbose_name в полях совпадает с ожидаемым."""
+        post = PostModelTest.post
+        field_verboses = {
+            'text': 'Текст поста',
+            'pub_date': 'Дата публикации',
+            'author': 'Автор',
+            'group': 'Группа',
+        }
+        for field, expected_value in field_verboses.items():
+            with self.subTest(field=field):
+                self.assertEqual(
+                    post._meta.get_field(field).verbose_name, expected_value)
+
+    def test_post_help_text(self):
+        """help_text в полях совпадает с ожидаемым."""
+        post = PostModelTest.post
+        field_help_texts = {
+            'text': 'Введите текст поста',
+            'group': 'Группа, к которой будет относиться пост',
+        }
+        for field, expected_value in field_help_texts.items():
+            with self.subTest(field=field):
+                self.assertEqual(
+                    post._meta.get_field(field).help_text, expected_value)
